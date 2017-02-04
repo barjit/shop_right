07683347bbeff4dbc60fc5a894930c28d310dd2c
Jeffrey Bartels <jeffreybartels@gmail.com>
2017-02-04 12:49:00 +1100

    home page test only

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
 
-  
-
-  resources :ingredient_quantities
-  resources :ingredients
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  
-  
 
   authenticated :user do
     root 'dashboard#index'
@@ -15,14 +9,14 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :meals
-
+  resources :ingredients
+  resources :ingredient_quantities
+  
   devise_for :users, controllers: {   
     sessions: 'users/sessions',   
     passwords: 'users/passwords',   
     registrations: 'users/registrations' 
   }
-
-
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
