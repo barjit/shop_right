7ee71f840f9c96ae519818de5a8f3a273b660990
Jeffrey Bartels <jeffreybartels@gmail.com>
2017-04-20 20:14:03 +1000

    fix issue deleting ingredients from meals

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,20 @@
+class IngredientsController < ApplicationController
+  def create
+    @meal = Meal.find(params[:meal_id])
+    @ingredient = @meal.ingredients.create(ingredients_params) 
+    redirect_to meal_path(@meal)
+  end
+
+  def destroy
+    @meal = Meal.find(params[:meal_id])
+    @ingredient = @meal.ingredients.find(params[:id])
+    @ingredient.destroy
+    redirect_to meal_path(@meal)
+  end
+
+  private
+
+    def ingredients_params
+      params.require(:ingredient).permit(:name, :unit, :quantity)
+    end
+end
