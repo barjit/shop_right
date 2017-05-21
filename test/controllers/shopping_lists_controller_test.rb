31faa1eec88d71643c042077d36e5a78aa2d9eb7
Jeffrey Bartels <jeffreybartels@gmail.com>
2017-05-21 19:38:57 +1000

    add shopping_list tests

--- test/controllers/shopping_lists_controller_test.rb
+++ test/controllers/shopping_lists_controller_test.rb
@@ -1,16 +1,41 @@
 require 'test_helper'
 
 class ShoppingListsControllerTest < ActionDispatch::IntegrationTest
-  include Devise::Test::ControllerHelpers
 
+  include Devise::Test::IntegrationHelpers
     setup do
-      sign_in @user, scope: :admin
+      sign_in users(:admin)
+      @shopping_list = shopping_lists(:one)
+    end
 
-      test "should get create" do
+      test "should get index" do
         get shopping_lists_url
         assert_response :success
       end
-  
-    end
 
+      test "should create shopping_list" do
+        assert_difference('ShoppingList.count') do
+          post shopping_lists_url, params: {shopping_list: {name: @shopping_list.name}}
+        end
+
+        assert_redirected_to edit_shopping_list_url(ShoppingList.last)
+      end
+
+      test "should show shopping_list" do
+        get shopping_list_url(@shopping_list)
+        assert_response :success
+      end
+
+      test "should get edit" do
+        get edit_shopping_list_url(@shopping_list)
+        assert_response :success
+      end
+
+      test "should destroy shopping_list" do
+        assert_difference('ShoppingList.count', -1) do
+          delete shopping_list_url(@shopping_list)
+        end
+
+        assert_redirected_to shopping_lists_url
+      end
 end
